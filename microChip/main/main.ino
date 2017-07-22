0e2ccf245eab3a4a64cc2367fbf919cc55770927
cromeG <christiangerhorst@gmail.com>
2017-07-22 16:42:01 +0200

    Print temperature from sensor to serial output The signal of a temperature sensor at port D1 can be read out. For this a special library with support for our board has been downloaded (see comments in code for detail). Furthermore, the interal libraries Base64 and OneWire are required. They are already installed when starting the Arduino IDE with the correct path ending with myWater in our case. While the internal LED is blinking the signal is read out and written to serial output. Configure serial output to a Baudrate of 115200.

--- microChip/main/main.ino
+++ microChip/main/main.ino
@@ -1,9 +1,59 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+ NodeMCU-DallasDS18B20
+ Led an dem Modul ESP8266 bzw. Board NodeMCU blinken lassen
+ und Temperatursensor an Pin D1 auslesen
+ 
+ Notwendig ist die angepasste Dallas-Lib: 
+   Download hier: https://github.com/milesburton/Arduino-Temperature-Control-Library
+   Eine eventuell vorhandene DallasTemperature-Lib sollte gelöscht werden, damit oben 
+   genannte von der IDE verwendet wird
+ 
+ Bezugsquelle Temperatursensor: Reichelt / Conrad / Amazon - http://amzn.to/2i3WlRX 
+ Bezugsquelle NodeMCU Board: http://amzn.to/2iRkZGi
 
+ Programm erprobt ab Arduino IDE 1.6.13
+ Weitere Beispiele unter http://www.mikrocontroller-elektronik.de/
+*/
+
+#define LED D0 //Interne Led auf dem NodeMCU Board LED_BUILTIN
+
+#include <DallasTemperature.h> //Siehe Hinweis oben, verwendet wird 
+                            //https://github.com/milesburton/Arduino-Temperature-Control-Library
+#include <Base64.h>
+#include <OneWire.h>
+
+#define ONE_WIRE_BUS D1  //Bestimmt Port an dem der Sensor angeschlossen ist
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature DS18B20(&oneWire);
+
+char temperaturStr[6];
+
+void setup() {
+ pinMode(LED, OUTPUT); // Port aus Ausgang schalten
+ Serial.begin(115200);
+ DS18B20.begin();
+}
+
+float getTemperatur() {
+ float temp;
+ do {
+ DS18B20.requestTemperatures(); 
+ temp = DS18B20.getTempCByIndex(0);
+ delay(100);
+ } while (temp == 85.0 || temp == (-127.0));
+ return temp;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ digitalWrite(LED, LOW); //Led port ausschalten
+ delay(1000); //1 Sek Pause
+ digitalWrite(LED, HIGH); //Led port einschlaten
+ delay(1000); 
+
+ float temperatur = getTemperatur();
+ dtostrf(temperatur, 2, 2, temperaturStr);
+ Serial.print("Temperatur: "); 
+ Serial.println(temperaturStr); 
 
 }
